9ffcf1d969c29df38b396a7894f6ddbb6968221e
Julian Kamphausen <juliankamphausen@me.com>
2022-06-26 17:04:30 +0200

    init + setup tailwind, bs icons, logos, colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mgred: '#E42321',
+        mgblue: '#008FDA',
+        mgyellow: '#FBDC4F',
+      }
+    },
+  },
+  plugins: [],
+}
